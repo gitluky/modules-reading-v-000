c5c34ec6dfbe619129ee25089d32e33a775d97de
Jonathan Luk <jluk87@gmail.com>
2018-03-24 22:29:18 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,5 +3,12 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+  include Dance
 
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  
 end
